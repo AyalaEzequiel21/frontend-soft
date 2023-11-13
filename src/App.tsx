57be5c6a9b119c4f6541632c56c37149f937aa6e
Checho <ezequielayala.c@gmail.com>
2023-11-13 18:33:05 -0300

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/login' element={<Login />}/>
+        <Route path='*' element={<Login />}/>
       </Routes>
     </BrowserRouter>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend-soft/"
+  base: "https://ayalaezequiel21.github.io/frontend-soft/"
 })
